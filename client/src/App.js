a64a80784c9e18a76da4f2785cd9286dd38a7b0a
Chris Baird <chrisbaird05@icloud.com>
2019-07-09 18:48:51 -0700

    Added folder structure for pages/components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import './App.css';
-import TestHome from './components/TestHome';
-import SettingsPage from './components/SettingsPage';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import LandingPage from './pages/landingPage';
+import SettingsPage from './pages/settingsPage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Landing Page</Link>
-          </li>
-          <li>
-            <Link to="/settings/">Settings</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Route path="/" exact component={TestHome} />
-      <Route path="/settings" component={SettingsPage} />
+      <Route path="/" exact component={LandingPage} />
+      <Route path="/settings/" component={SettingsPage} />
     </Router>
   );
 }
