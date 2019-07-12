5ff38d72ac2a688d96c725ba993f25ce8b0766f3
Chris Baird <chrisbaird05@icloud.com>
2019-07-11 18:41:06 -0700

    Set up env usage

--- controllers/yelpController.js
+++ controllers/yelpController.js
@@ -1,6 +1,7 @@
 // Yelp Controller
 module.exports = {
   test: (req, res) => {
+    console.log(process.env.MESSAGE);
     res.send('Welcome to test');
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
