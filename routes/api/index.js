507af43c9f5965fa57bf67a537e9f18908491d7f
Chris Baird <chrisbaird05@icloud.com>
2019-08-06 14:50:40 -0700

    Removed comments.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,8 @@ const path = require('path');
 const router = require('express').Router();
 const yelpRoutes = require('./yelp');
 
-// Yelp Routes
 router.use('/yelp', yelpRoutes);
 
-// For anything else, render the html page
 router.use((req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
