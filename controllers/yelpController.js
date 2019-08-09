fadc0e016cfc87b97b50cd8ceb73eb5e05e44557
Chris Baird <chrisbaird05@icloud.com>
2019-08-09 13:12:50 -0700

    Working Validation Checkpoint

--- controllers/yelpController.js
+++ controllers/yelpController.js
@@ -5,12 +5,8 @@ module.exports = {
     console.log(
       '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
     );
-    const { latitude, longitude, radius, term = 'food' } = req.body;
+    const { latitude, longitude, location, radius, term = 'food' } = req.body;
     console.log(req.body);
-
-    console.log(typeof latitude);
-    console.log(typeof longitude);
-    console.log(typeof radius);
 
     console.log(
       '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
@@ -20,30 +16,30 @@ module.exports = {
       radius
     )}&categories=restaurants,all&term=${term}&open_now=true&limit=50`;
 
-    axios({
-      method: 'GET',
-      url: query,
-      headers: {
-        Authorization: 'bearer ' + process.env.YELP_API_KEY
-      }
-    })
-      .then(results => {
-        const yelpData = results.data.businesses;
+    // axios({
+    //   method: 'GET',
+    //   url: query,
+    //   headers: {
+    //     Authorization: 'bearer ' + process.env.YELP_API_KEY
+    //   }
+    // })
+    //   .then(results => {
+    //     const yelpData = results.data.businesses;
 
-        function Places(item) {
-          this.id = item.id || null;
-          this.imageUrl = item.image_url || null;
-          this.name = item.name || null;
-          this.phone = item.phone || null;
-          this.price = item.price || null;
-          this.rating = item.rating || null;
-          this.location = item.location || null;
-        }
+    //     function Places(item) {
+    //       this.id = item.id || null;
+    //       this.imageUrl = item.image_url || null;
+    //       this.name = item.name || null;
+    //       this.phone = item.phone || null;
+    //       this.price = item.price || null;
+    //       this.rating = item.rating || null;
+    //       this.location = item.location || null;
+    //     }
 
-        const formattedData = yelpData.map(item => new Places(item));
+    //     const formattedData = yelpData.map(item => new Places(item));
 
-        res.json(formattedData);
-      })
-      .catch(err => console.log(err));
+    //     res.json(formattedData);
+    //   })
+    //   .catch(err => console.log(err));
   }
 };
